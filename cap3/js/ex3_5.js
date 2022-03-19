d0bedfa8ab57b7344d7f13482aef18dadf3a13f8
pisanidaniel <dfpisani@gmail.com>
2022-03-19 20:25:14 -0300

    add ex3_5 exercise

---
+++ cap3/js/ex3_5.js
@@ -0,0 +1,29 @@
+function calcularRaiz() {
+    // cria um referência aos elementos da página
+    var inNumero = document.getElementById("inNumero");
+    var outResposta = document.getElementById("outResposta");
+
+    // obtém conteúdo do campo inNumero
+    var numero = Number(inNumero.value);
+
+    // se não preencheu ou Not-a-Number(NaN)
+    if (numero == 0 || isNaN(numero)) {
+        alert("Informe um número válido..."); //exibe alerta
+        inNumero.focus(); // posiciona no campo inNumero
+        return; // retorna
+    }
+
+    var raiz = Math.sqrt(numero); // calcula raiz quadrada do número
+
+    // se valor da variável raiz igual a este valor arredondado para baixo...
+    if(raiz == Math.floor(raiz)) {
+        outResposta.textContent = "Raiz " + raiz; // mostra a raiz
+    } else {
+        // senão, exibe mensagem indicando que não há raiz exata
+        outResposta.textContent = "Não há raiz exata para" + numero;
+    }
+}
+
+// cria referência ao elemento btExibir e registra evento que irá carregar function
+var btExibir = document.getElementById("btExibir");
+btExibir.addEventListener("click", calcularRaiz);
