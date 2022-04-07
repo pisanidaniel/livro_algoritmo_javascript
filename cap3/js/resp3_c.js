ad8c950a3cba1754e64c97f5d3a5fcb25a2ef6a1
pisanidaniel <dfpisani@gmail.com>
2022-04-06 21:26:17 -0300

    add resp3.c

--- cap3/js/resp3_c.js
+++ cap3/js/resp3_c.js
@@ -1,27 +1,48 @@
-function confirmaDeposito() {
+function verificarMulta() {
     // cria uma referência aos elementos da página
     var inValor = document.getElementById("inValor");
     var outTempo = document.getElementById("outTempo");
-    var outSituacao = document.getElementById("outSituacao");
+    var outTroco = document.getElementById("outSituacao");
 
-    // 
+    // obtém conteúdo do campo de entrada
     var valor = Number(inValor.value);
 
     // se não preencheu ou Not-a-Number (NaN)
-    if (inValor.value == "" || isNaN(valor) ||
-        inValor.value == "" || isNaN(valor)) {
+    if (inValor.value == 0 || isNaN(valor)) {
         // exibe mensagem de advertência
-        alert("Informe as velocidades corretamente...");
+        alert("Informe um valor válido de moedas");
         // posiciona no campo de edição inVelPermitida
         inValor.focus();
         return;
     }
-    
-    // cria as condições para verificar situação do condutor
-    if (inValor == 1.00)
 
+    // caso valor insuficiente
+    if (valor < 1.00) {
+        alert("Valor Insuficiente (no mínimo, R$ 1.00)");
+        inValor.focus();
+        return;
+    }
 
+    // declara variáveis
+    var tempo;
+    var troco;
 
+    if (valor >= 3.00) {
+        tempo = 120;
+        troco = valor - 3.00;
+    } else if (valor >= 1.75) {
+        tempo = 60;
+        troco = valor - 1.75;
+    } else {
+        tempo = 30;
+        troco = valor - 1.00;
+    }
 
-
+    outTempo.textContent = "Tempo: " + tempo + "min";
+    if (troco > 0) {
+        ouTroco.textContent = "Troco R$: " + troco.toFixed(2);
+    }
 }
+
+var btnConfirmar = document.getElementById("btnConfirmar");
+btnConfirmar.addEventListener("click", verificarMulta);
